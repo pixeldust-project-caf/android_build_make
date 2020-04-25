LineageOS/android_build

soong: include Lineage soong_config
Change-Id: Ic7b8993f4a201f8a9d24c6ba32617fa34843afc2

core/soong_config.mk
@@ -151,6 +151,8 @@ $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
   $(eval _contents := $$(_contents)$(if $(strip $(SOONG_CONFIG_$(namespace))),__SV_END)        },$$(newline)))
 _contents := $(_contents)$(if $(strip $(SOONG_CONFIG_NAMESPACES)),__SV_END)    },$(newline)
 
+include vendor/lineage/build/soong/soong_config.mk
+
 _contents := $(subst $(comma)$(newline)__SV_END,$(newline),$(_contents)__SV_END}$(newline))
 
 $(file >$(SOONG_VARIABLES).tmp,$(_contents))

pixeldust-project-caf/android_build_make

core/soong_config.mk
@@ -179,6 +179,8 @@ $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
   $(call end_json_map))
 $(call end_json_map)
 
+include vendor/pixeldust/build/soong/soong_config.mk
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))